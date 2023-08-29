09f80791b232b8de3c977d51b1aa5631437b911d
kammryndancy <kammryn.dancy@gmail.com>
2023-08-29 00:02:15 -0600

    Add photography section

---
+++ src/app/components/photogrpahy/photoexamples.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'photoexamples',
+  templateUrl: './photoexamples.component.html',
+  styleUrls: ['./photoexamples.component.scss'],
+  standalone: true
+})
+export class PhotoexamplesComponent {}
