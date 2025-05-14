e0de1572ab6772dde76320e0d7aa1022d05d27ed
Kammryn dancy <kammryn.dancy@gmail.com>
2025-05-14 15:05:34 -0600

    Build out pages

--- src/app/roaster/roaster.component.ts
+++ src/app/roaster/roaster.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 
 import { FooterComponent } from "../components/footer/footer.component";
 import { LandingComponent } from "../components/landing/landing.component";
+
+interface RoastEquipment {
+  name: string;
+  description: string;
+  image: string;
+  link?: string;
+}
+
+interface RoastHistory {
+  date: string;
+  beanOrigin: string;
+  roastLevel: string;
+  image: string;
+  taste: string;
+}
 
 @Component({
   selector: 'roaster',
@@ -10,7 +26,52 @@ import { LandingComponent } from "../components/landing/landing.component";
   imports: [
     LandingComponent,
     FooterComponent,
+    NgFor,
+    NgIf
   ],
   standalone: true
 })
-export class RoasterComponent {}
+export class RoasterComponent {
+  equipment: RoastEquipment[] = [
+    {
+      name: 'Auvelcraft Infrared Coffee Roaster',
+      description: 'Used over natural gas stove, this tiny machine produces a fantastic and consistent roast in batches of 50g - 200g. Taking between 11-15 minutes to roast a batch depending on desired roast level.',
+      image: '/assets/images/coffee/coffee_roaster.avif',
+      link: 'https://www.auvelcraft.co.jp/'
+    },
+    {
+      name: 'Metal Cooling Tray',
+      description: 'Simple barbeque vegetable roaster used as a cooling tray with integrated fan system to rapidly halt the roasting process and preserve bean flavor.',
+      image: '/assets/images/coffee/coffee_colombian_tray.jpg'
+    },
+    {
+      name: 'Natural Gas Stove',
+      description: 'A simple natural gas stove used to heat up the roasters steel mesh plate in order to emit infrared heat towards the coffee beans.',
+      image: '/assets/images/coffee/coffee_colombian_tray.jpg'
+    }
+  ];
+
+  roastHistory: RoastHistory[] = [
+    {
+      date: '2024-03-15',
+      beanOrigin: 'Colombian High Altitude',
+      roastLevel: 'First Crack',
+      image: '/assets/images/coffee/coffee_colombian_tray.jpg',
+      taste: 'Chocolate, Blackberry, Nutty'
+    },
+    {
+      date: '2024-04-22',
+      beanOrigin: 'Ethiopian Yirgacheffe',
+      roastLevel: 'City/First Crack+/First Crack/Green',
+      image: '/assets/images/coffee/coffee_yirgacheffe_jars.jpg',
+      taste: 'Citrus, Floral, Nutty, Molasses'
+    },
+    {
+      date: '2024-05-10',
+      beanOrigin: 'Brazilian Anaerobic Honey',
+      roastLevel: 'First Crack+',
+      image: '/assets/images/coffee/brazilian_honey.jpg',
+      taste: 'Citrus, Floral, Oranges, Tannic'
+    }
+  ];
+}
--- src/app/woodwork/woodworking.component.ts
+++ src/app/woodwork/woodworking.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { FooterComponent } from "../components/footer/footer.component";
 import { LandingComponent } from "../components/landing/landing.component";
+import { ProjectListComponent, ProjectEntry } from '../components/project/project-list.component';
 
 @Component({
   selector: 'woodworking',
@@ -10,7 +11,37 @@ import { LandingComponent } from "../components/landing/landing.component";
   imports: [
     LandingComponent,
     FooterComponent,
+    ProjectListComponent,
   ],
   standalone: true
 })
-export class WoodworkingComponent {}
+export class WoodworkingComponent {
+  projects: ProjectEntry[] = [
+    {
+      images: [
+        'assets/images/woodwork/d20_flower_mat.jpg',
+        'assets/images/woodwork/d20_flower_projection.jpg',
+        'assets/images/woodwork/d20_dragon_1.jpg',
+        'assets/images/woodwork/d20_dragon_2.jpg',
+        'assets/images/woodwork/d20_dragon_blue.jpg'
+      ],
+      title: 'D20 Lamp - Laser Cut/Engraved',
+      description: 'Not entirely orginal but believe it or not I did design it from the ground up. The lamp stands at about 20cm tall and wide. Cut using 1.5mm plywood and assembled with UV resin with a wifi connected LED light inside. The first one was made with a flower motif and sproudly sits on my desk. The second made as a gift was adorned with a dragon motif.'
+    },
+    {
+      images: ['assets/images/woodwork/sprout_stand_full.jpg', 'assets/images/woodwork/sprout_stand_side.jpg', 'assets/images/woodwork/sprout_stand_detail.jpg'],
+      title: 'Sprouting Jar Stand',
+      description: 'Made using oak wood and brass rods, this stand is meant to allow seeds to drain and promote their sprouting. The stand is about 15cm tall, 20cm deep and 35cm wide. The internal structure was sized to allow a small tray to sit and gather draining water. Simple but effective.'
+    },
+    {
+      images: ['assets/images/woodwork/ChristmasOrnament.jpg'],
+      title: 'Christmas Ornaments - Laser Cut/Engraved',
+      description: 'Made using 1.5mm plywood, wrapping paper, twine, artificial flowers, and UV resin. Made for the members of theDungeons and Dragons campaign I am DMing. The ornaments are about 10cm tall and wide. Merry Critmas!'
+    },
+    {
+      images: ['assets/images/woodwork/dice_tower_closed.jpg', 'assets/images/woodwork/dice_tower_open.jpg'],
+      title: 'Dice Tower - Laser Cut/Engraved',
+      description: 'A rather large dice tower made using 3.5mm red oak plywood. This is not my design but instead downloaded from Etsy. The tower is about 25cm tall and 15cm wide. This tower was made as a gift.'
+    }
+  ];
+}
